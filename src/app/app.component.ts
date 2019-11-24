a57f438187d48bf39d023a8eb878836b7de99ac6
Gabriela <gabriela.nucci@dilettasolutions.com>
2019-11-24 20:40:59 -0300

    Seja bem vindo trazendo nome do cliente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, Input } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { AuthService } from './auth/auth.service';
 import { Observable } from 'rxjs';
 import { LocalUser } from './auth/credentials/local_user';
+import { PessoaService } from './pessoa/pessoa.service';
 
 @Component({
   selector: 'app-root',
@@ -13,17 +14,33 @@ export class AppComponent {
 
   authenticated$: Observable<boolean>;
   user$: Observable<LocalUser>;
+  @Input() nome: string;
+  pessoa: any;
 
   constructor(
     private router: Router,
-    private authService: AuthService){
+    private authService: AuthService, 
+    public buscaEmail: PessoaService){
       this.authenticated$ = this.authService.isAuthenticated();
       this.user$ = this.authService.getUser();
-      this.router = router;
   }
 
   ngOnInit(){
     this.authenticated$ = this.authService.isAuthenticated();
+    
+    const token = localStorage.getItem("localUser")
+    var obj = JSON.parse(token)
+
+     this.buscaEmail.buscarEmailLoginConjunto(obj.email).subscribe((perfil) => {
+        this.pessoa = perfil
+
+        if(this.pessoa.perfil == "CLIENTE"){
+          this.nome = this.pessoa.nomeCompleto;
+        }
+        if(this.pessoa.perfil == "SERVICO"){
+          this.nome = this.pessoa.razaoSocial;
+        }
+    });
   }
 
   login(){
